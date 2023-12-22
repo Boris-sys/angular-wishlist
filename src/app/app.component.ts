5c89182c71f37d04db1726daa8474fa01cf913b9
boris-sys <borissicsac@gmail.com>
2023-12-22 15:27:57 -0300

    Wishilist initial structure, Data Model, and Binding Events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,50 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/models/wishItem';
 
+const filters = [
+  (item : WishItem) => item,
+  (item : WishItem) => !item.isComplete,
+  (item : WishItem) => item.isComplete
+];
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
+  items : WishItem[] = [
+    new WishItem('Learn Angular'),
+    new WishItem('Join sprint planning meeting', true),
+    new WishItem('Develop Denodo lab')
+  ]
+
+  
+
+  listFilter : any = '0';
+
+  newWishText  = '';
+
   title = 'wishlist';
+
+  get visibleItems() : WishItem[] {
+    return this.items.filter(filters[this.listFilter]);
+  };
+
+  addNewWish() {
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = '';
+  }
+
+  toggleItem(item : WishItem) {
+    item.isComplete = !item.isComplete;
+    console.log(item);
 }
+}
+

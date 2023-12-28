dd89d0b1a247ac71384b7026f97c4b4864c68681
boris-sys <borissicsac@gmail.com>
2023-12-28 16:55:52 -0300

    Added some features: Using Observables to Create an Event Bus. Injecting Dependencies. Issuing HTTP Requests using Angular built-in http client. Handling Errors for HTTP Requests. Creating Modules to organize related components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,14 @@
-import { Component } from '@angular/core';
-import { WishItem } from 'src/shared/models/wishItem';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  items : WishItem[] = [
-    new WishItem('Learn Angular'),
-    new WishItem('Get Coffee', true),
-    new WishItem('Find grass that cuts itself')
-  ];
+export class AppComponent implements OnInit {
+  constructor() { }
 
-  
-  filter: any; 
+  ngOnInit(): void {
+  }
 }
